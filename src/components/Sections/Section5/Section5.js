443a8969eec5abe603850e9d0e3e3cda684e1fa2
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-04-17 15:23:28 +0500

    slider done

--- src/components/Sections/Section5/Section5.js
+++ src/components/Sections/Section5/Section5.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Section5.css"
 import TreeView from "./TreeView"
+import Slider from "./Slider"
 function Section5() {
     return (
         <div className="section5">
           <div>  
               <h1 style={{color:"grey"}}>What you can expect</h1>
-              <div><img style={{width:"80%"}} src="https://www.lyonaeroports.com/sites/default/files/styles/adl_medium/public/2020-05/athenes-header.jpg?itok=9xp3IjJs"></img></div>
+              <div><Slider /></div>
+               
              <p style={{color:"grey",fontSize:"15px",marginTop:"8px"}}>Discover the history and the myths of Ancient Athens on a private tour with a local guide. Admire the best<br></br>
              places related to Greek Gods and hear the tales behind...<span style={{color:"pink"}}>read more</span>
 
---
+++ src/components/Sections/Section6/Modal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Card from "./Card"
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+       <div onClick={handleOpen}><Card /></div>
+      {/* <button type="button" onClick={handleOpen}>
+      <Card />
+      </button> */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+       <img style={{width:"50%"}} src="https://www.lyonaeroports.com/sites/default/files/styles/adl_medium/public/2020-05/athenes-header.jpg?itok=9xp3IjJs"></img>
+      </Modal>
+    </div>
+  );
+}
